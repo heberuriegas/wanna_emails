30a04114ab9f91b316303c5cc6b7878501af5b32
Heber Garza <heber.fernando@gmail.com>
2013-09-21 15:42:38 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WannaEmails::Application.config.secret_key_base = '3e4c0eb55ed3f7fbacf031f0ceca0654c214f967948bb48072fdd5ef65768ba19449465680ca9461f35fea351f7f17905fa13a5cd84df3ae9e79fcfcaffeaafa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WannaEmails::Application.config.session_store :cookie_store, key: '_wanna_emails_session'
