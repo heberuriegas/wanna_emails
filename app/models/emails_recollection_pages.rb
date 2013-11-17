087166c44b83d36a3d525613d094f3dccac0e358
Heber Garza <heber.fernando@gmail.com>
2013-11-17 01:49:44 -0300

    Improve counter cache

--- app/models/emails_recollection_pages.rb
+++ app/models/emails_recollection_pages.rb
@@ -1,6 +1,14 @@
 class EmailsRecollectionPages < ActiveRecord::Base
   belongs_to :email
-  belongs_to :recollection_page, counter_cache: true
+  belongs_to :recollection_page
 
   self.primary_key = [:email_id, :recollection_page_id]
+
+  after_save :update_counter_cache
+  after_destroy :update_counter_cache
+
+  def update_counter_cache
+    self.recollection_page.update_attribute :emails_recollection_pages_count, self.recollection_page.emails.uniq{|email| email.address}.count
+  end
 end
+  
