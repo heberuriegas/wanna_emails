4e4d54dac24175e0da81283c087548a882910d86
Heber Garza <heber.fernando@gmail.com>
2013-11-28 14:07:54 -0300

    Add module to tor privoxy

---
+++ lib/wanna_emails/proxy.rb
@@ -0,0 +1,19 @@
+module WannaEmails
+  module Proxy
+    def self.included(base)
+      
+      require "capybara"
+      require "capybara/dsl"
+      require "capybara-webkit"
+
+      Capybara.default_wait_time = 10
+      Capybara.current_driver = :selenium
+
+      include Capybara::DSL
+
+      agent = TorPrivoxy::Agent.new host: ENV['TOR_HOST'], password: ENV['TOR_PASSWORD'], privoxy_port: ENV['TOR_PRIVOXY_PORT'], control_port: ENV['TOR_CONTROL_PORT'], capybara: true do |agent|
+        logger.info "New IP is #{agent.ip}"
+      end
+      
+  end
+end 
