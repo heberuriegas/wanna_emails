43b12a3f16281900678beef7f0c5a4bb853cbc08
Heber Garza <heber.fernando@gmail.com>
2013-12-04 12:07:21 -0300

    Add production smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ WannaEmails::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.logger = Logger.new("log/mailer.log")
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
