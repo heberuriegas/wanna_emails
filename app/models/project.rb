744b8693283df8dfa864e48a2c9a966f50c85997
Heber Uriegas <heber.fernando@gmail.com>
2016-10-22 10:53:12 -0500

    Add clean project

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,36 @@ class Project < ActiveRecord::Base
   has_many :messages
 
   validates :name, presence: true
+
+  def clean
+    _campaigns = self.campaigns.all
+    _recollections = []
+    _recollection_pages = []
+    _pages = []
+    _prospects = []
+
+    _recollections << self.recollections
+
+    _campaigns.each do |campaign|
+      _recollections << campaign.recollections
+    end
+
+    _recollections.each do |recollection|
+      recollection.each do |_recollection|
+        _recollection.recollection_pages.each do |recollection_page|
+          _recollection_pages << recollection_page
+        end
+      end
+    end
+
+    _recollection_pages.each do |recollection_page|
+      _pages << recollection_page.page
+      _prospects << recollection_page.prospects
+    end
+
+    _pages.each { |t| t.destroy }
+    _prospects.each { |t| t.destroy }
+    _recollection_pages.each { |t| t.destroy }
+    _recollections.each { |t| t.destroy }
+  end
 end
